aa78e91082f178218d0042401c76b12f4dab3046
SlInevit <slmozart2003@gmail.com>
2025-11-05 22:58:58 +0800

    Ready to push.

--- test/modop-test.cu
+++ test/modop-test.cu
@@ -13,12 +13,54 @@ using namespace std;
 using namespace alt_bn128;
 typedef libsnark::default_r1cs_ppzksnark_pp ppT;
 
+#define ADD_TIMES 2
+
 void modadd_compute(TestGPULayout<fr_t, fr_t> &gpu_layout)
 {
     size_t n = gpu_layout.n;
     kernel<<<n / 512, 512>>>([=] __device__ (fr_t *array, fr_t *result) {
         int tid = threadIdx.x + blockIdx.x * blockDim.x;
-        result[tid] = array[2 * tid] + array[2 * tid + 1];
+        fr_t acc = array[2 * tid], inc = array[2 * tid + 1];
+        #pragma unroll
+        for (int i = 0; i < ADD_TIMES; i++) acc += inc;
+        result[tid] = acc;
+    }, gpu_layout.device_array, gpu_layout.device_result);
+    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaGetLastError());
+}
+
+__device__ __forceinline__ void dis_add8(uint32_t &a, const uint32_t &b, int &disoff)
+{
+    uint32_t g, p, g_, p_;
+    asm("add.cc.u32 %0, %0, %1;" : "+r"(a) : "r"(b));
+    asm("addc.u32 %0, 0, 0;" : "=r"(g));
+    p = (a == -1);
+    for (int i = 1; i < 8; i <<= 1) {
+        g_ = __shfl_up_sync(0xffffffff, g, 1);
+        p_ = __shfl_up_sync(0xffffffff, p, 1);
+        if (disoff >= 2 * i) {
+            g |= (g_ & p);
+            p &= p_;
+        }
+    }
+    g = __shfl_up_sync(0xffffffff, g, 1);
+    if (disoff > 0) a += g;
+}
+
+void modadd_compute2(TestGPULayout<fr_t, fr_t> &gpu_layout)
+{
+    size_t n = gpu_layout.n;
+    kernel<<<n * 8 / 512, 512>>>([=] __device__ (fr_t *array, fr_t *result) {
+        int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        int disid = tid / 8, disoff = tid % 8;
+        uint32_t acc, inc;
+        acc = reinterpret_cast<uint32_t*>(array + 2 * disid)[disoff];
+        inc = reinterpret_cast<uint32_t*>(array + 2 * disid + 1)[disoff];
+        #pragma unroll
+        for (int i = 0; i < ADD_TIMES; i++) {
+            dis_add8(acc, inc, disoff);
+        }
+        reinterpret_cast<uint32_t*>(result + disid)[disoff] = acc;
     }, gpu_layout.device_array, gpu_layout.device_result);
     cudaDeviceSynchronize();
     CUDA_CHECK(cudaGetLastError());
@@ -29,40 +71,8 @@ void modmul_compute(TestGPULayout<fr_t, fr_t> &gpu_layout)
     size_t n = gpu_layout.n;
     kernel<<<n / 512, 512>>>([=] __device__ (fr_t *array, fr_t *result) {
         int tid = threadIdx.x + blockIdx.x * blockDim.x;
-        result[tid] = array[2 * tid] * array[2 * tid + 1];
-    }, gpu_layout.device_array, gpu_layout.device_result);
-    cudaDeviceSynchronize();
-    CUDA_CHECK(cudaGetLastError());
-}
-
-void my_modmul(TestGPULayout<fr_t, fr_t> &gpu_layout)
-{
-    const size_t block_size = 512;
-    size_t n = gpu_layout.n;
-    kernel<<<n / block_size, block_size>>>([=] __device__ (fr_t *array, fr_t *result) {
-        // ASSUME sizeof(fr_t) = 8 * sizeof(uint32_t)
-        int tid = threadIdx.x + blockIdx.x * blockDim.x;
-        int warpid = tid / 32, laneid = tid % 32;
-        int warpid_in_block = threadIdx.x / 32;
-        // warp j is going to load target = array[64j : 64j + 63]
-        __shared__ fr_t buffer[block_size * 2];
-        // which can be seen as an array of uint4 with size 64 * 2 = 128
-        fr_t *buffer_ptr = buffer + warpid_in_block * 64;
-        fr_t *array_ptr = array + warpid * 64;
-        for (int i = laneid; i < 128; i += 32) reinterpret_cast<uint4*>(buffer_ptr)[i] = reinterpret_cast<uint4*>(array_ptr)[i];
-        __syncwarp();
-
-        fr_t read[2];
-        #pragma unroll
-        for (int i = 0; i < 4; i++) {
-            int indice = (i + laneid / 2) % 4;
-            reinterpret_cast<uint4*>(read)[indice] = reinterpret_cast<uint4*>(buffer_ptr + 2 * laneid)[indice];
-        }
-        __syncwarp();
-        
-        read[0] *= read[1];
-        result[tid] = read[0];
-
+        fr_t read[2] = {array[2 * tid], array[2 * tid + 1]};
+        result[tid] = read[0] * read[1];
     }, gpu_layout.device_array, gpu_layout.device_result);
     cudaDeviceSynchronize();
     CUDA_CHECK(cudaGetLastError());
@@ -74,22 +84,31 @@ int main(int argc, char *argv[])
 
     ppT::init_public_params();
 
-    int t = 10;
-    if (argc > 0) t = atoi(argv[1]);
+    int t = 1;
+    if (argc > 1) t = atoi(argv[1]);
     while (t--) {
 
-        if (0) {
+        if (1) {
             Tester<libff::Fr<ppT>, libff::Fr<ppT>> tester(
                 n,
-                [] (libff::Fr<ppT> &x) { x = libff::Fr<ppT>::random_element(); },
-                [] (const libff::Fr<ppT> &x, const libff::Fr<ppT> &y) { return x + y; },
+                [] (libff::Fr<ppT> &x) { 
+                    x = libff::Fr<ppT>::random_element();
+                    x.mont_repr.data[x.num_limbs - 1] >>= 1;
+                },
+                [] (const libff::Fr<ppT> &x, const libff::Fr<ppT> &y) { 
+                    libff::Fr<ppT> s = x;
+                    #pragma unroll
+                    for (int i = 0; i < ADD_TIMES; i++) s += y;
+                    return s; 
+                },
                 "Addition Under Module"
             );
-            TestGPULayout<fr_t, fr_t> gpu_layout;
+            TestGPULayout<fr_t, fr_t> gpu_layout, gpu_layout2;
             tester.gpu_bench(gpu_layout, test_gpu_setup<libff::Fr<ppT>, fr_t, fr_t>, modadd_compute);
+            tester.gpu_bench(gpu_layout2, test_gpu_setup<libff::Fr<ppT>, fr_t, fr_t>, modadd_compute2);
         }
 
-        if (1) {
+        if (0) {
             Tester<libff::Fr<ppT>, libff::Fr<ppT>> tester(
                 n,
                 [] (libff::Fr<ppT> &x) { x = libff::Fr<ppT>::random_element(); },
@@ -98,18 +117,6 @@ int main(int argc, char *argv[])
             );
             TestGPULayout<fr_t, fr_t> gpu_layout;
             tester.gpu_bench(gpu_layout, test_gpu_setup<libff::Fr<ppT>, fr_t, fr_t>, modmul_compute);
-            
-        }
-
-        if (1) {
-            Tester<libff::Fr<ppT>, libff::Fr<ppT>> tester(
-                n,
-                [] (libff::Fr<ppT> &x) { x = libff::Fr<ppT>::random_element(); },
-                [] (const libff::Fr<ppT> &x, const libff::Fr<ppT> &y) { return x * y; },
-                "Multiplication Under Module"
-            );
-            TestGPULayout<fr_t, fr_t> gpu_layout;
-            tester.gpu_bench(gpu_layout, test_gpu_setup<libff::Fr<ppT>, fr_t, fr_t>, my_modmul);
         }
 
     }
